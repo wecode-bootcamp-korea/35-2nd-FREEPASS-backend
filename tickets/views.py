6af775d6e434016a0e664634f9b97ab911e08a25
WorkYong <pjyong01@gmail.com>
2022-08-04 17:39:27 +0900

    add : search_view

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+
+from tickets.models   import Ticketing, Airline, Airplane, Location , Discount
+from tickets.models   import FlightInformation, SeatClass, Passenger, PassengerType, FlightDetail
+
+class FlightLocationView(View):
+    def get(self, request):
+        locations = Location.objects.all()
+    
+        result = [{
+                'id'       : location.id,
+                'name'     : location.name,
+                'code'     : location.code,  
+        } for location in locations]
+
+        return JsonResponse({"result":result}, status=200)
